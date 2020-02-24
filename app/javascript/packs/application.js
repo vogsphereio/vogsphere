57aa5ef2c3802cbd958b1ad35c5076913e46e396
Andrew Brown <andrewbrown@Andrews-MacBook-Pro.local>
2020-02-24 11:22:53 -0500

    added social nav bar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 import ('./bootstrap_custom.js');
 import('stylesheets/application.scss');
+import "@fortawesome/fontawesome-free/js/all";
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
