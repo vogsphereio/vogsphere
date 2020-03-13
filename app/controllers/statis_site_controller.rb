709071ef5c9bac8467a2e8fb396c597fb97f4d8d
Andrew Brown <andrewbrown@Andrews-MacBook-Pro.local>
2020-03-13 10:51:54 -0400

    CSS fixes

--- app/controllers/statis_site_controller.rb
+++ app/controllers/statis_site_controller.rb
@@ -7,6 +7,9 @@ class StatisSiteController < ApplicationController
 
   def resources
   end
+  
+  def partners
+  end
 
   def about
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'statis_site/index'
   get 'statis_site/usecases'
   get 'statis_site/resources'
+  get 'statis_site/partners'
   get 'statis_site/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
